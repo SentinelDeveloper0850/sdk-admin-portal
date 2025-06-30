b23afa89cfb97481cf2b9875fa18ff301ed17426
SentinelDeveloper0850 <givensomdaka@gmail.com>
2025-06-30 12:10:40 +0200

    fix: update styling on loading indicator

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,7 +5,9 @@ import { Spin } from "antd";
 export default function Loading() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-white dark:bg-[#212121]">
-      <Spin size="large" tip="Loading..." />
+      <div className="flex justify-center items-center">
+        <Spin size="large" tip="Loading..." />
+      </div>
       <h1 className="mb-4 text-4xl font-bold text-gray-800 dark:text-white">
         Loading
       </h1>
