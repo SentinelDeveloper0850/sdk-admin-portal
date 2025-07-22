d8ac4f34c124521f8532c6d4c5888e9b8702b277
SentinelDeveloper0850 <givensomdaka@gmail.com>
2025-07-22 14:01:23 +0200

    fix: Module not found: Can't resolve '@/app/components/import-tools/pdf/statement-analyzer'

--- src/app/(protected)/transactions/eft/analyze/page.tsx
+++ src/app/(protected)/transactions/eft/analyze/page.tsx
@@ -2,7 +2,6 @@
 
 import { Button, Col, Row, Space, Statistic } from "antd";
 
-import PDFBankStatementAnalyzer from "@/app/components/import-tools/pdf/statement-analyzer";
 import PageHeader from "@/app/components/page-header";
 
 const EftPdfAnalysis = () => {
@@ -26,25 +25,9 @@ const EftPdfAnalysis = () => {
               <Statistic title="Selected Transactions" value={0} />
             </Space>
           </Col>
-          {/* <Col
-            span={12}
-            style={{ display: "flex", justifyContent: "flex-end" }}
-          >
-            {user?.role == "admin" && (
-              <Space>
-                <BankStatementExcelImporter />
-                <TransactionHistoryCsvImporter
-                  handleChange={fileChangeHandler}
-                  allowMultiple
-                  label="Import Transaction History (CSV)"
-                />
-              </Space>
-            )}
-          </Col> */}
         </Row>
       </PageHeader>
 
-      <PDFBankStatementAnalyzer />
     </div>
   );
 };
