c385994a182ba4576851e4a2c49883c1dd15289c
SentinelDeveloper0850 <givensomdaka@gmail.com>
2025-12-15 11:55:55 +0200

    hide dev indicators

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: {
     buildActivity: false,
-    buildActivityPosition: "bottom-right",
   },
   eslint: {
     ignoreDuringBuilds: true,
